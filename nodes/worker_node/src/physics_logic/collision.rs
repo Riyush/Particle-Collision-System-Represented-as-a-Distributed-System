99da014f103501ff0bbaed450109a090b76a47f6
Daniel Golonka <danielgolonka@Daniels-MacBook-Air.local>
2025-01-02 19:04:35 -0600

    Skeleton and collision function descrpition

---
+++ nodes/worker_node/src/physics_logic/collision.rs
@@ -0,0 +1,7 @@
+
+// Collision Inputs:
+// Two instances of BaseParticle struct
+
+// Collision Outputs:
+// Resultant transverse energy E; final particle velocity/transverse momentum
+// and position; particle type?; cross section?
